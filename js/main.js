ee5f215793a886e7478ac979a090be52a3dd14ab
Miumio <nickolskaya@gmail.com>
2021-02-11 00:34:40 +0200

    внесла правки в функции для временных данных

--- js/main.js
+++ js/main.js
@@ -22,16 +22,20 @@ const getRandomCoordinates = function (x, y, digits) {
 
 getRandomCoordinates(1, 100, 2);
 
-const typeOfObject = [
+const TYPES_OF_ObJECT = [
   'palace',
   'flat',
   'house',
   'bungalow',
-]
+];
 
-let checkTime = String(getRandomNumber(12, 14)) + ':00';
+const CHECK_TIMES = [
+  '12:00',
+  '13:00',
+  '14:00',
+];
 
-let objectFeatures = [
+const OBJECT_FEATURES = [
   'wifi',
   'dishwasher',
   'parking',
@@ -40,41 +44,50 @@ let objectFeatures = [
   'conditioner',
 ];
 
-let objectPhotos = [
+const OBJECT_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
 ];
 
-const getRandomArrayElement = (elements) => {
-  return elements[getRandomNumber(0, elements.length - 1)];
+const getRandomArrayElement = (array) => {
+  return array[getRandomNumber(0, array.length - 1)];
 };
 
-const getNotRepeatItem = (arr) => {
-  return arr.slice(0, getRandomNumber(0, arr.length - 1));
+const shuffle = (arr) => {
+  let temp;
+  for (let i = arr.length - 1; i > 0; i--) {
+    let k = Math.floor(Math.random() * (i + 1));
+    temp = arr[k];
+    arr[k] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
 };
 
 const createObject = () => {
+  let locationX = getRandomCoordinates(35.65000, 35.70000, 5);
+  let locationY = getRandomCoordinates(139.70000, 139.80000, 5);
   return {
     author: {
       avatar: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png',
     },
     offer: {
       title: 'строка — заголовок предложения',
-      address: 'x: ' + getRandomCoordinates(35.65000, 35.70000, 5)  + ' y: ' + getRandomCoordinates(139.70000, 139.80000, 5),
+      address: 'x: ' + locationX  + ', y: ' + locationY,
       price: getRandomNumber(0, 1000000000),
-      type: getRandomArrayElement(typeOfObject),
+      type: getRandomArrayElement(TYPES_OF_ObJECT),
       rooms: getRandomNumber(0, 100),
       guests: getRandomNumber(0, 100),
-      checkin: checkTime,
-      checkout: checkTime,
-      features: getNotRepeatItem(objectFeatures),
+      checkin: getRandomArrayElement(CHECK_TIMES),
+      checkout: getRandomArrayElement(CHECK_TIMES),
+      features: shuffle(OBJECT_FEATURES),
       description: 'строка — описание помещения',
-      photos: getRandomArrayElement(objectPhotos),
+      photos: getRandomArrayElement(OBJECT_PHOTOS),
     },
     location: {
-      x: getRandomCoordinates(35.65000, 35.70000, 5),
-      y: getRandomCoordinates(139.70000, 139.80000, 5),
+      x: locationX,
+      y: locationY,
     },
   };
 };
