be50cf1e688eed37615c4127f7b95734d640a8e8
Miumio <nickolskaya@gmail.com>
2021-02-12 12:31:53 +0200

    починила ошибку Cannot use import statement outside a module

--- js/util.js
+++ js/util.js
@@ -6,8 +6,6 @@ const getRandomNumber = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomNumber(1, 11);
-
 const getRandomCoordinates = function (x, y, digits) {
   if (x >= y) {
     return -1;
@@ -16,7 +14,9 @@ const getRandomCoordinates = function (x, y, digits) {
   return (Math.random() * (y - x) + x).toFixed(digits);
 }
 
-getRandomCoordinates(1, 100, 2);
+const getRandomArrayElement = (array) => {
+  return array[getRandomNumber(0, array.length - 1)];
+};
 
 const shuffle = (arr) => {
   let temp;
@@ -28,12 +28,6 @@ const shuffle = (arr) => {
   }
   return arr;
 };
-shuffle();
-
-const getRandomArrayElement = (array) => {
-  return array[getRandomNumber(0, array.length - 1)];
-};
-getRandomArrayElement();
 
 const getNotRepeatItem = (arr) => {
   const randomNumber = getRandomNumber(0, arr.length - 1);
@@ -41,7 +35,5 @@ const getNotRepeatItem = (arr) => {
   shuffle(newArray);
   return newArray;
 };
-
-getNotRepeatItem();
 
 export {getRandomNumber, getRandomCoordinates, shuffle, getRandomArrayElement, getNotRepeatItem};
