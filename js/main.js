4df97d91a6eb1c976388cf4b9951494b590b6d43
Miumio <nickolskaya@gmail.com>
2021-02-12 11:53:26 +0200

    добавила util js

--- js/main.js
+++ js/main.js
@@ -1,25 +1,3 @@
-'use strict';
-
-const getRandomNumber = function (min, max) {
-  if (min < 0 || min > max) {
-    return -1;
-  }
-
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-getRandomNumber(1, 11);
-
-const getRandomCoordinates = function (x, y, digits) {
-  if (x >= y) {
-    return -1;
-  }
-
-  return (Math.random() * (y - x) + x).toFixed(digits);
-}
-
-getRandomCoordinates(1, 100, 2);
-
 const TYPES_OF_OBJECT = [
   'palace',
   'flat',
@@ -50,17 +28,6 @@ const OBJECT_PHOTOS = [
 
 const getRandomArrayElement = (array) => {
   return array[getRandomNumber(0, array.length - 1)];
-};
-
-const shuffle = (arr) => {
-  let temp;
-  for (let i = arr.length - 1; i > 0; i--) {
-    let k = Math.floor(Math.random() * (i + 1));
-    temp = arr[k];
-    arr[k] = arr[i];
-    arr[i] = temp;
-  }
-  return arr;
 };
 
 const getNotRepeatItem = (arr) => {
