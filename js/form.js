d830977711320148100796c13842fc68a5021c06
Miumio <nickolskaya@gmail.com>
2021-02-17 12:49:07 +0200

    disabled map filter

--- js/form.js
+++ js/form.js
@@ -4,5 +4,13 @@ form.classList.add('ad-form--disabled');
 const fieldset = form.querySelectorAll('fieldset');
 
 for (let i = 0; i < fieldset.length; i++) {
-  fieldset[i].setAttribute('disabled', '')
+  fieldset[i].setAttribute('disabled', '');
+};
+
+const filters = document.querySelector('.map__filters');
+filters.classList.add('ad-form--disabled');
+
+const select = filters.querySelectorAll('select');
+for (let i = 0; i < select.length; i++) {
+  select[i].setAttribute('disabled', '');
 }
