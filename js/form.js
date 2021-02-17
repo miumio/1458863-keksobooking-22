097e0795ff3118f6778a8195f773e50c4953b04f
Miumio <nickolskaya@gmail.com>
2021-02-17 12:17:06 +0200

    add form js

---
+++ js/form.js
@@ -0,0 +1,8 @@
+const form = document.querySelector('.ad-form');
+form.classList.add('ad-form--disabled');
+
+const fieldset = form.querySelectorAll('fieldset');
+
+for (let i = 0; i < fieldset.length; i++) {
+  fieldset[i].setAttribute('disabled', '')
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,2 @@
 import './similar-objects-list.js';
-
-const form = document.querySelector('.ad-form');
-form.classList.add('ad-form--disabled');
-
-const fieldset = form.querySelectorAll('fieldset');
-
-for (let i = 0; i < fieldset.length; i++) {
-  fieldset[i].setAttribute('disabled', '')
-}
+import './form.js';
