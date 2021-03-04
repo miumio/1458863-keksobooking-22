8ff22ad0b736ca2c10824b3a7cc278545a533f0b
Miumio <nickolskaya@gmail.com>
2021-03-04 13:27:07 +0200

    add connection error msg👻

--- js/data.js
+++ js/data.js
@@ -1,12 +1,26 @@
 const URL = 'https://22.javascript.pages.academy/keksobooking/data';
 
+const errorMessage = () => {
+  const popup = document.createElement('div');
+
+  popup.textContent = '!WARNING! conection error';
+  popup.classList.add('error__getdata');
+
+  document.body.appendChild(popup);
+
+  setTimeout (() => {
+    popup.remove();
+  }, 3000)
+};
+
 const getData = (onSucces) => {
   fetch(URL)
   .then((response) => response.json())
   .then((objects) => {
     onSucces(objects);
   })
-  .catch(() => {  });
+  .catch(() => {errorMessage()});
 };
+
 
 export {getData};
