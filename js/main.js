820a2c5e0f664986d4d93c46792c5b79750fcf0a
Miumio <nickolskaya@gmail.com>
2021-03-18 14:41:51 +0200

    add debounce

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
+/* global _:readonly */
+
 import {createPins} from './map.js';
 import {getData} from './data.js';
 import {changeFilter} from './filter.js';
 import {getOn, formReset} from './form.js';
 
+const RERENDER_DELAY = 500;
+
 getData((objects) => {
   createPins(objects);
   getOn();
   formReset (() => createPins(objects))
-  changeFilter(() => createPins(objects));
+  changeFilter(_.debounce(() => createPins(objects), RERENDER_DELAY,));
 });
